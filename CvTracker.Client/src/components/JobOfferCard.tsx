977effc30a0364df2d20bc7af8447f30262f0adc
Kacper Kotecki <kacperkotecki@o2.pl>
2026-05-02 21:16:14 +0200

    refactor: Improve navigate

--- CvTracker.Client/src/components/JobOfferCard.tsx
+++ CvTracker.Client/src/components/JobOfferCard.tsx
@@ -12,7 +12,7 @@ export default function JobOfferCard({ jobOffer }: JobOfferCardProps) {
   const navigate = useNavigate()
 
   const goToEditOfferById = (id : number) => {
-  navigate(`./edit/${id}`)
+  navigate(`/edit/${id}`)
 }
 
 
